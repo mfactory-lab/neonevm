5d30dc9b14649dda2b707922002a3034a6da0b3d
tiamo <vk.tiamo@gmail.com>
2024-01-25 13:39:01 +0200

    fix: add token list

---
+++ src/tokens.ts
@@ -0,0 +1,12 @@
+export default {
+  JSOL: {
+    chainId: 245022926, // devnet
+    // chainId: 245022934, // mainnet
+    address_spl: 'F8s94y4EBKiYPGuXzzik2TXaGwSa29K1w3d5xvEEzcDx',
+    address: '0x2cc12c6477cA4F4b703c81670C3a85993A6464Ef',
+    decimals: 9,
+    name: 'JPOOL Solana Token',
+    symbol: 'JSOL',
+    logoURI: 'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/7Q2afV64in6N6SeZsAAB81TJzwDoD6zpqmHkzi9Dcavn/logo.svg',
+  },
+}
